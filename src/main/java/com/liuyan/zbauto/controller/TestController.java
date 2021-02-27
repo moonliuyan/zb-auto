503cbde9fcf5e11e0409dc605ae7f5477efeedcf
moonlight <liuyan_moon@126.com>
2021-02-27 10:39:21 +0800

    fix

--- src/main/java/com/liuyan/zbauto/controller/TestController.java
+++ src/main/java/com/liuyan/zbauto/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.liuyan.zbauto.controller;
 
+import com.liuyan.zbauto.mapper.entity.TradeRecord;
 import com.liuyan.zbauto.service.TradeRecordService;
 import com.liuyan.zbauto.wechat.WechatService;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/liuyan/zbauto/wechat/WechatServiceImpl.java
+++ src/main/java/com/liuyan/zbauto/wechat/WechatServiceImpl.java
@@ -1,9 +1,17 @@
 package com.liuyan.zbauto.wechat;
 
+
+import com.alibaba.fastjson.JSONObject;
+import com.liuyan.zbauto.mapper.WxAccessTokenMapper;
+import com.liuyan.zbauto.mapper.entity.WxAccessToken;
 import com.liuyan.zbauto.util.HttpUtilManager;
+import org.apache.http.HttpException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.LinkedHashMap;
 
 @Service
@@ -14,17 +22,52 @@ public class WechatServiceImpl implements WechatService {
     private String secret;
     @Value("${wechat.accessUrl}")
     private String accessUrl;
+    @Autowired
+    WxAccessTokenMapper wxAccessTokenMapper;
 
 
     public String getWeChatAccessTokenUrl() {
         String json="";
+        WxAccessToken wxAccessToken = wxAccessTokenMapper.selectByPrimaryKey(1);
         try {
-            String url = accessUrl.replace("APPID",appId).replace("APPSECRET",secret);
-            json = HttpUtilManager.getInstance().requestHttpGet(url, new LinkedHashMap<>());
+
+            if(wxAccessToken!=null){
+                Date updateTime = wxAccessToken.getUpdateTime();
+                if(new Date().getTime()-updateTime.getTime()>2*60*60*1000){
+                    String accessToken = getAccessToken();
+                    wxAccessToken.setAccessToken(accessToken);
+                    wxAccessToken.setUpdateTime(new Date());
+                    wxAccessTokenMapper.updateByPrimaryKey(wxAccessToken);
+                }
+            }else{
+                String accessToken = getAccessToken();
+                wxAccessToken=new WxAccessToken();
+                wxAccessToken.setAccessToken(accessToken.toString());
+                wxAccessToken.setCreateTime(new Date());
+                wxAccessToken.setUpdateTime(new Date());
+                wxAccessTokenMapper.insert(wxAccessToken);
+
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             return "请求获取access_token出现异常";
         }
-        return json;
+        return wxAccessToken.getAccessToken();
+    }
+
+    private String getAccessToken(){
+        String url = accessUrl.replace("APPID",appId).replace("APPSECRET",secret);
+        String json = null;
+        try {
+            json = HttpUtilManager.getInstance().requestHttpGet(url, new LinkedHashMap<>());
+        } catch (HttpException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        Object accessToken = jsonObject.get("access_token");
+        return accessToken.toString();
     }
 }
